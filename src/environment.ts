58b25123af29155c748ea1213da0c72a83f701de
Vina del Mar <vina@duoc.acad>
2025-06-02 19:39:06 -0400

    aws

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 const url = {
-  apiUrl: "http://localhost:3000/apiBsnss",
-  apiUrlUsers: "http://localhost:5000/apiUsers",
+  apiUrl: "http://integest.michimisimo.cloud/apiBsnss",
+  apiUrlUsers: "http://integest.michimisimo.cloud/apiUsers",
 };
 
 export default url;
