90c5081e7b07453e0af2098a6c782a17d9693e7d
Vina del Mar <vina@duoc.acad>
2025-06-02 20:28:52 -0400

    aws5

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 const url = {
-  apiUrl: "https://inteplat.michimisimo.cloud:3000/apiBsnss",
-  apiUrlUsers: "https://inteplat.michimisimo.cloud:5000/apiUsers",
+  apiUrl: "http://inteplat.michimisimo.cloud:3000/apiBsnss",
+  apiUrlUsers: "http://inteplat.michimisimo.cloud:5000/apiUsers",
 };
 
 export default url;
