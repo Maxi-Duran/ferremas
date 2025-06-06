4650c6f3091c21960c1a02e5a78537cad2ec8c51
Maximiliano Duran <maximiliano.duran114@gmail.com>
2025-06-06 14:31:32 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/apiBsnss": {
+        target: "http://inteplat.michimisimo.cloud:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/apiUsers": {
+        target: "http://inteplat.michimisimo.cloud:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
